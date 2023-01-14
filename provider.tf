80c97d8dba12d9b2adedef4d3c3bd1d676491004
rssh-jp <t-araumi@rssh.sakura.ne.jp>
2023-01-14 09:25:04 +0900

    commit

--- provider.tf
+++ provider.tf
@@ -1,8 +1,3 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
